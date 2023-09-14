48fbccda7a87d5e71c509e5e4c0dc87877d90574
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-09-14 23:49:08 +0600

    code is done but need to secure some edge cases

--- insert_in_array.c
+++ insert_in_array.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
 int main()
 {
-	printf("Hello world");
+	int array[100] = {1, 2, 3, 4, 5};
+	int arrayLength = 5;
+	int position, value;
+	int guard;
+pos:
+	printf("Position: ");
+	scanf("%d", &position);
+	if (position > arrayLength + 1)
+	{
+		printf("Positioned can not be more than %d\n", arrayLength + 1);
+		goto pos;
+	}
+
+	printf("Value: ");
+	scanf("%d", &value);
+	printf("The value must be an integer\n");
+
+	printf("Old Array: ");
+	for (int i = 0; i < arrayLength; i++)
+	{
+		printf("%d ", array[i]);
+	}
+
+	for (int i = arrayLength; i >= position; i--)
+	{
+		array[i] = array[i - 1];
+	}
+	array[position - 1] = value;
+
+	printf("\nUpdated Array: ");
+	for (int i = 0; i < arrayLength + 1; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	return 0;
 }

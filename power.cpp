abe661f37e76811363472df0883f96390d0114f0
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-09-16 09:38:51 +0600

    power of 2

---
+++ power.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int powerOf2(int n)
+{
+	if (n == 0)
+		return 1;
+
+	return 2 * powerOf2(n - 1);
+}
+
+int main()
+{
+	int n, nsquared;
+	cin >> n;
+
+	nsquared = powerOf2(n);
+	cout << nsquared << endl;
+}
